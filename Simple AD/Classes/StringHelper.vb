c7d8fd26386d5194cf9bb7b2dc621196fdfc3501
Joel Crosby <joelcrosby@live.co.uk>
2017-08-14 10:16:59 +0100

    Delete StringHelper.vb

--- Simple AD/Classes/StringHelper.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class StringHelper
-    Public Shared Function GetFileNameShort(ByVal Filename As String) As String
-        Dim StringArryay = Filename.Split(New Char() {"\"})
-        Return StringArryay(StringArryay.Count - 1)
-    End Function
-End Class
